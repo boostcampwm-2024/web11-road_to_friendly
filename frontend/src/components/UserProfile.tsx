a76f0708983c3d6dd380ba6763d9f62f4f954596
choyunhee <xornd1110@naver.com>
2024-11-05 22:59:31 +0900

    feat: 🎸 방장 및 참여자 페이지 구분, RoomNotFound 페이지 생성

---
+++ frontend/src/components/UserProfile.tsx
@@ -0,0 +1,11 @@
+const UserProfile = () => {
+    return (
+      <div>
+        <div>프로필</div>
+        <div>유저네임</div>
+      </div>
+    );
+  };
+  
+  export default UserProfile;
+  
---
+++ frontend/src/pages/room/participantView.tsx
@@ -0,0 +1,10 @@
+const ParticipantView = () => {
+  return (
+    <p>
+      <div>참여자 화면</div>
+      <h2> 방장이 시작 버튼을 누르지 않았어요. 조금만 더 기다려주세요</h2>
+    </p>
+  );
+};
+
+export default ParticipantView;

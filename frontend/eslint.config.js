629c6667536900212657a05d91d48ad16dfb4284
minsuhan1 <trms7794han@gmail.com>
2024-11-05 13:24:17 +0900

    feat: 🎸 eslint + prettier 설정 추가, 폴더구조 설정

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,28 +1,34 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: globals.browser
     },
     plugins: {
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      'react-refresh': reactRefresh
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-)
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      // TypeScript 관련 설정
+      '@typescript-eslint/no-explicit-any': 'warn', // any 사용 경고
+      '@typescript-eslint/no-unused-vars': 'error', // 사용되지 않는 변수 경고
+
+      // React 관련 설정
+      'react/react-in-jsx-scope': 'off', // Reat 17+ 이상에서 React import 없이 사용 가능
+      'react/prop-types': 'off', // TS 사용 시 PropTypes 생략
+      'no-debugger': 'off', // debugger 사용 가능
+      'no-console': 'off' // console 사용 가능
+    }
+  }
+);

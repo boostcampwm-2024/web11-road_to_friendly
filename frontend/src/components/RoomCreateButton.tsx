26d6bf9a61ae1d3f88de5e16a153cd6bc3ca8ee5
choyunhee <xornd1110@naver.com>
2024-11-06 11:58:27 +0900

    feat: 🎸 방 개설 api 연결

---
+++ frontend/src/components/RoomCreateButton.tsx
@@ -0,0 +1,34 @@
+import { useNavigate } from 'react-router-dom';
+
+const RoomCreateButton = () => {
+  const navigate = useNavigate();
+
+  const createRoom = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/rooms', {
+        method: 'POST',
+        body: JSON.stringify({ test: true }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      if (response.status === 200) {
+        const roomUrl = new URL(response.url);
+        if (roomUrl) navigate(roomUrl.pathname);
+      } else {
+        console.error('Failed to create room');
+      }
+    } catch (error) {
+      console.error('Error creating room:', error);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={createRoom}>방 개설</button>
+    </div>
+  );
+};
+
+export default RoomCreateButton;

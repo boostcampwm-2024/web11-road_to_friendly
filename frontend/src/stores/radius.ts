d2fbff48f735dc37496e4e21e774649d3499da2f
choyunhee <xornd1110@naver.com>
2024-11-12 19:20:17 +0900

    feat: 🎸 결과 페이지의 키워드 배치를 위한 increaseLongRadius 추가

--- frontend/src/stores/radius.ts
+++ frontend/src/stores/radius.ts
@@ -5,9 +5,14 @@ import { MIN_SHORT_RADIUS, MIN_LONG_RADIUS, MAX_SHORT_RADIUS, MAX_LONG_RADIUS } 
 interface RadiusStore {
   radius: [number, number];
   increaseRadius: () => void;
+  increaseLongRadius: () => void;
 }
 
-export const useRadiusStore = create<RadiusStore>((set) => ({
+export const useRadiusStore = create<RadiusStore>((set, get) => ({
   radius: [MIN_SHORT_RADIUS, MIN_LONG_RADIUS],
-  increaseRadius: () => set({ radius: [MAX_SHORT_RADIUS, MAX_LONG_RADIUS] })
+  increaseRadius: () => set({ radius: [MAX_SHORT_RADIUS, MAX_LONG_RADIUS] }),
+  increaseLongRadius: () => {
+    const currentRadius = get().radius;
+    set({ radius: [currentRadius[0], MAX_LONG_RADIUS + 100] });
+  }
 }));

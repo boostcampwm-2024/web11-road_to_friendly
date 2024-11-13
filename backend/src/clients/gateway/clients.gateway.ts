06c79338dfa7fa9856f7acca2354a5a578302bc4
Jongmin Lee <em11133@naver.com>
2024-11-13 21:33:35 +0900

    refactor: clients gateway 추가 분리

--- backend/src/clients/gateway/clients.gateway.ts
+++ backend/src/clients/gateway/clients.gateway.ts
@@ -1,4 +1,11 @@
-import { SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  WsException,
+} from '@nestjs/websockets';
 import { ClientsService } from '../service/clients.service';
 import { Server, Socket } from 'socket.io';
 
@@ -13,4 +20,17 @@ export class ClientsGateway {
   server: Server;
 
   constructor(private readonly clientsService: ClientsService) {}
+
+  @SubscribeMessage('client:update')
+  updateClientInfo(@ConnectedSocket() client: Socket, @MessageBody() { nickname }): void {
+    const roomId = client.data.roomId;
+
+    if (roomId === undefined) {
+      throw new WsException('방에 참가하지 않으셨습니다.');
+    }
+
+    client.data.nickname = nickname;
+
+    this.server.to(roomId).emit('participant:info:update', { participantId: client.id, nickname });
+  }
 }

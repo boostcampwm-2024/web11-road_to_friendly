09d78b446566daf38fa9530aafe1af0621cc5a1c
Jongmin Lee <em11133@naver.com>
2024-11-13 22:53:45 +0900

    chore: app.module guard 추가

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,11 +7,31 @@ import { RoomsController } from './rooms/controller/rooms.controller';
 import { EmpathyGateway } from './empathy/gateway/empathy.gateway';
 import { EmpathyService } from './empathy/service/empathy.service';
 import { EmpathyInMemoryRepository } from './empathy/repository/empathy.in-memory.repository';
+import { ClientsGateway } from './clients/gateway/clients.gateway';
+import { ClientsService } from './clients/service/clients.service';
+import { RoomsInMemoryRepository } from './rooms/repository/rooms.in-memory.repository';
+import { HostGuard } from './rooms/guard/rooms.host.guard';
+import { JoinGuard } from './rooms/guard/rooms.join.guard';
+import { ExistGuard } from './rooms/guard/rooms.exist.guard';
+import { ConnectionGuard } from './rooms/guard/rooms.connection.guard';
 
 @Module({
   imports: [],
   controllers: [AppController, RoomsController],
-  providers: [AppService, RoomsGateway, RoomsService, EmpathyGateway, EmpathyService, EmpathyInMemoryRepository],
+  providers: [
+    AppService,
+    RoomsGateway,
+    RoomsService,
+    RoomsInMemoryRepository,
+    HostGuard,
+    JoinGuard,
+    ExistGuard,
+    ConnectionGuard,
+    EmpathyGateway,
+    EmpathyService,
+    EmpathyInMemoryRepository,
+    ClientsGateway,
+    ClientsService,
+  ],
 })
-export class AppModule {
-}
+export class AppModule {}

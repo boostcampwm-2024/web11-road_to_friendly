dedf451e0a529fbba7ec31e2d98aa75ce620ff88
23YUJIN <asdbnm066@daum.net>
2024-11-14 16:43:54 +0900

    refactor: 💡 keywords store에서 불필요한 요소 삭제
    counts는 처음에는 정렬용으로 만들었던 객체지만, 정렬을 keywords를 통해 하면 필요가 없기 때문에 삭제함

--- frontend/src/stores/keywords.ts
+++ frontend/src/stores/keywords.ts
@@ -10,37 +10,27 @@ interface Count {
   [count: number]: number;
 }
 
-interface Counts {
-  [questionId: number]: number[];
-}
-
 interface CountMap {
   [questionId: number]: Count;
 }
 
 interface KeywordsStore {
   keywords: Keywords;
-  counts: Counts;
   countMap: CountMap;
+
   upsertKeyword: (targetKeyword: KeywordInfo) => void;
   deleteKeyword: (targetKeyword: { questionId: number; keyword: string }) => void;
 }
 
-function appendArrayUnique<T>(array: T[], element: T): T[] {
-  if (!array.includes(element)) return [...array, element];
-  return array;
-}
-
-function syncCountMap(array: number[], countMap: Count, element: number): Count {
-  if (!array.includes(element)) {
-    return { ...countMap, [element]: 1 };
+function syncCountMap(countMap: Count, count: number): Count {
+  if (!Object.keys(countMap).includes(count.toString())) {
+    return { ...countMap, [count]: 1 };
   }
   return countMap;
 }
 
 function appendKeyword(
   keywords: Keywords,
-  counts: Counts,
   countMap: CountMap,
   newKeywordData: { questionId: number; keyword: string; count: number }
 ) {
@@ -50,14 +40,12 @@ function appendKeyword(
       ...keywords,
       [questionId]: [...(keywords[questionId] || []), { keyword, count: 1 }]
     },
-    counts: { ...counts, [questionId]: appendArrayUnique(counts[questionId] || [], 1) },
-    countMap: { ...countMap, [questionId]: syncCountMap(counts[questionId] || [], countMap[questionId] || {}, 1) }
+    countMap: { ...countMap, [questionId]: syncCountMap(countMap[questionId] || {}, 1) }
   };
 }
 
 function modifyKeywordCount(
   keywords: Keywords,
-  counts: Counts,
   countMap: CountMap,
   targetKeyword: { questionId: number; keyword: string; count: number }
 ) {
@@ -73,42 +61,26 @@ function modifyKeywordCount(
       ...keywords,
       [questionId]: sortedKeywords
     },
-    counts: { ...counts, [questionId]: appendArrayUnique(counts[questionId] || [], count).sort((a, b) => b - a) },
-    countMap: { ...countMap, [questionId]: syncCountMap(counts[questionId] || [], countMap[questionId] || {}, count) }
+    countMap: { ...countMap, [questionId]: syncCountMap(countMap[questionId] || {}, count) }
   };
 }
 
 export const useKeywordsStore = create<KeywordsStore>((set) => ({
   keywords: {
-    1: [
-      { keyword: '김치찌개', count: 1 },
-      { keyword: '떡볶이', count: 1 },
-      { keyword: '제육볶음', count: 1 },
-      { keyword: '미역국', count: 1 },
-      { keyword: '삼계탕', count: 1 }
-    ],
-    2: [
-      { keyword: '만두전골', count: 1 },
-      { keyword: '비빔밥', count: 1 },
-      { keyword: '불고기', count: 1 },
-      { keyword: '닭칼국수다 어쩔래어쩔어쩔', count: 1 },
-      { keyword: '장충동왕족발보쌈', count: 1 },
-      { keyword: '찜닭', count: 1 },
-      { keyword: '갈비탕', count: 1 }
-    ],
+    1: [],
+    2: [],
     3: [],
     4: [],
     5: []
   },
-  counts: { 1: [1] },
-  countMap: { 1: { 1: 5 } },
+  countMap: {},
 
   upsertKeyword: (targetKeyword) => {
     set((state) => {
       const { questionId, keyword } = targetKeyword;
       if (state.keywords[questionId].some((element) => element.keyword === keyword))
-        return modifyKeywordCount(state.keywords, state.counts, state.countMap, targetKeyword);
-      return appendKeyword(state.keywords, state.counts, state.countMap, targetKeyword);
+        return modifyKeywordCount(state.keywords, state.countMap, targetKeyword);
+      return appendKeyword(state.keywords, state.countMap, targetKeyword);
     });
   },
 

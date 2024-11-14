c907fedef67cde584fe061c30db8d40feee8ab2a
eora21 <eora21@naver.com>
2024-11-14 15:14:27 +0900

    test: 💍 테스트 통과하도록 종속성 추가

--- backend/src/rooms/repository/rooms.in-memory.repository.spec.ts
+++ backend/src/rooms/repository/rooms.in-memory.repository.spec.ts
@@ -1,6 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { RoomPhase, RoomsInMemoryRepository } from './rooms.in-memory.repository';
-import { v4 as uuid } from 'uuid';
+import { ROOM_PHASE, RoomsInMemoryRepository } from './rooms.in-memory.repository';
 
 jest.mock('uuid', () => ({
   v4: jest.fn().mockReturnValue('mocked-uuid'),
@@ -23,65 +22,65 @@ describe('RoomsGateway', () => {
 
   describe('create', () => {
     it('create는 roomId를 반환한다', async () => {
-      const roomId = await repository.create();
+      const roomId = repository.create();
 
       expect(roomId).toBe('mocked-uuid');
-      expect(await repository.isExistRoom(roomId)).toBe(true);
+      expect(repository.isExistRoom(roomId)).toBe(true);
     });
   });
 
   describe('isExistRoom', () => {
     it('방이존재한다면 true를 반환한다', async () => {
-      const roomId = await repository.create();
-      expect(await repository.isExistRoom(roomId)).toBe(true);
+      const roomId = repository.create();
+      expect(repository.isExistRoom(roomId)).toBe(true);
     });
 
     it('방이존재하지 않으면 false를 반환한다', async () => {
-      expect(await repository.isExistRoom('undifined')).toBe(false);
+      expect(repository.isExistRoom('undifined')).toBe(false);
     });
   });
 
   describe('join', () => {
     it('첫번쨰로 방에 접속한사람이 host가 된다.', async () => {
-      const roomId = await repository.create();
+      const roomId = repository.create();
       const clientId = 'client1';
 
-      const hostId = await repository.join(roomId, clientId);
+      const hostId = repository.setHostIfHostUndefined(roomId, clientId);
       expect(hostId).toBe(clientId);
-      expect(await repository.isHost(roomId)).toBe(clientId);
+      expect(repository.getHostId(roomId)).toBe(clientId);
     });
 
     it('나중에 방에 접속한사람이 host가 되지않아야 한다.', async () => {
-      const roomId = await repository.create();
+      const roomId = repository.create();
       const firstClientId = 'client1';
       const secondClientId = 'client2';
 
-      await repository.join(roomId, firstClientId);
-      const hostId = await repository.join(roomId, secondClientId);
+      repository.setHostIfHostUndefined(roomId, firstClientId);
+      const hostId = repository.setHostIfHostUndefined(roomId, secondClientId);
 
       expect(hostId).toBe(firstClientId); // 첫 번째 클라이언트가 호스트여야 함
-      expect(await repository.isHost(roomId)).toBe(firstClientId);
+      expect(repository.getHostId(roomId)).toBe(firstClientId);
     });
   });
 
   describe('changeRoomPhase', () => {
     it('방에 페이즈를 변경한다', async () => {
-      const roomId = await repository.create();
+      const roomId = repository.create();
 
-      await repository.changeRoomPhase(roomId, RoomPhase.IN_PROGRESS);
+      repository.changeRoomPhase(roomId, ROOM_PHASE.IN_PROGRESS);
 
-      const room = await repository.isExistRoom(roomId);
+      const room = repository.isExistRoom(roomId);
       expect(room).toBe(true);
-      expect(await repository['rooms'].get(roomId).phase).toBe(RoomPhase.IN_PROGRESS);
+      expect(repository['rooms'].get(roomId).phase).toBe(ROOM_PHASE.IN_PROGRESS);
     });
   });
 
   describe('deleteRoom', () => {
     it('should delete a room if it exists', async () => {
-      const roomId = await repository.create();
+      const roomId = repository.create();
 
-      await repository.deleteRoom(roomId);
-      expect(await repository.isExistRoom(roomId)).toBe(false);
+      repository.deleteRoom(roomId);
+      expect(repository.isExistRoom(roomId)).toBe(false);
     });
   });
 });

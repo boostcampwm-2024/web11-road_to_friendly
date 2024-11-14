35fa15463f1799e83a0110cd3862a94e1ef1334e
23YUJIN <asdbnm066@daum.net>
2024-11-15 04:36:40 +0900

    feat: 🎸 키워드 count에 따른 그룹화, 그룹에 따른 크기 조절 및 애니메이션

--- frontend/src/pages/room/KeywordsView.tsx
+++ frontend/src/pages/room/KeywordsView.tsx
@@ -3,45 +3,79 @@ import { useEffect } from 'react';
 
 import { useSocketStore } from '@/stores';
 import { useKeywordsStore } from '@/stores/keywords';
-import { Variables } from '@/styles';
+import { keywordStyleMap, Variables } from '@/styles';
+import { Group, Keyword, PrefixSum } from '@/types';
+import { BIG_THRESHOLD, MIDEIUM_THRESHOLD, SMALL_THRESHOLD } from '@/constants';
 
 const KeywordsContainer = css`
   width: 100%;
   margin-top: 50px;
   display: flex;
   justify-content: center;
+  align-items: center;
   flex-wrap: wrap;
   gap: 8px;
 `;
+
 const KeywordStyle = css`
   border: 1px solid black;
-  padding: 3px 10px;
+  height: fit-content;
+  padding: 0.5rem 0.75rem;
   background: ${Variables.colors.surface_word_default};
-  border-radius: 50px;
   text-align: center;
   min-width: 60px;
+  transition: font-size 0.3s ease;
 `;
 
 interface KeywordsViewProps {
   questionId: number;
 }
 
+function getKeywordGroup(keyword: Keyword, keywordCountSum: number, prefixSum: PrefixSum): Group {
+  const { count } = keyword;
+  const curPrefixSum = prefixSum[count];
+  const ratio = Math.ceil((curPrefixSum / keywordCountSum) * 100);
+
+  if (ratio < BIG_THRESHOLD) {
+    return 'Big';
+  } else if (ratio < MIDEIUM_THRESHOLD) {
+    return 'Medium';
+  } else if (ratio < SMALL_THRESHOLD) {
+    return 'Small';
+  }
+  return 'Tiny';
+}
+
 const KeywordsView = ({ questionId }: KeywordsViewProps) => {
   const { socket } = useSocketStore();
-  const { keywords, upsertKeyword } = useKeywordsStore();
+  const { keywords, prefixSumMap, upsertKeyword } = useKeywordsStore();
 
   useEffect(() => {
     if (socket) {
+      socket.off('empathy:keyword:count');
+
       socket.on('empathy:keyword:count', (response: { questionId: number; keyword: string; count: number }) => {
         upsertKeyword(response);
       });
     }
+
+    return () => {
+      socket?.off('empathy:keyword:count');
+    };
   }, [socket]);
 
   return (
     <div css={KeywordsContainer}>
       {keywords[questionId]?.map((keywordObject) => (
-        <div key={`${questionId}-${keywordObject.keyword}`} css={KeywordStyle}>
+        <div
+          key={`${questionId}-${keywordObject.keyword}`}
+          css={[
+            KeywordStyle,
+            keywordStyleMap(false)[
+              getKeywordGroup(keywordObject, keywords[questionId].length, prefixSumMap[questionId])
+            ]
+          ]}
+        >
           {keywordObject.keyword}
         </div>
       ))}

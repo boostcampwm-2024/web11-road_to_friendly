c3d26555f733492c261804c443029378edc0bdba
eora21 <eora21@naver.com>
2024-11-14 15:18:02 +0900

    refactor: 💡 (WsExceptionFilter) 콜백 인덱스 상수화, response 미리 정의

--- backend/src/rooms/filter/rooms.filter.ts
+++ backend/src/rooms/filter/rooms.filter.ts
@@ -1,17 +1,19 @@
 import { ArgumentsHost, Catch } from '@nestjs/common';
 import { BaseWsExceptionFilter, WsException } from '@nestjs/websockets';
 
+const CALLBACK_INDEX = 2;
+
 @Catch(WsException)
 export class WsExceptionFilter extends BaseWsExceptionFilter {
   catch(exception: WsException, host: ArgumentsHost) {
     const client = host.switchToWs().getClient();
-    const args = host.getArgs();
-    const callback = args[2];
+    const callback = host.getArgByIndex(CALLBACK_INDEX);
+    const response = { status: 'error', message: exception.message };
 
     if (typeof callback === 'function') {
-      callback({ status: 'error', message: exception.message });
+      callback(response);
     } else {
-      client.emit('error', { status: 'error', message: exception.message });
+      client.emit('error', response);
     }
   }
 }

3831cfbd15ed8820540dbaaa169ac0ad88d954a0
eora21 <eora21@naver.com>
2024-11-14 17:02:10 +0900

    refactor: 💡 (ClientsGateway) 닉네임 업데이트 시 ConnectGuard 사용

--- backend/src/clients/gateway/clients.gateway.ts
+++ backend/src/clients/gateway/clients.gateway.ts
@@ -1,12 +1,7 @@
-import {
-  ConnectedSocket,
-  MessageBody,
-  SubscribeMessage,
-  WebSocketGateway,
-  WebSocketServer,
-  WsException,
-} from '@nestjs/websockets';
+import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer, } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
+import { UseGuards } from '@nestjs/common';
+import { ConnectGuard } from '../../common/guard/connect.guard';
 
 @WebSocketGateway({
   cors: {
@@ -18,14 +13,10 @@ export class ClientsGateway {
   @WebSocketServer()
   server: Server;
 
+  @UseGuards(ConnectGuard)
   @SubscribeMessage('client:update')
   updateClientInfo(@ConnectedSocket() client: Socket, @MessageBody() { nickname }): void {
     const roomId = client.data.roomId;
-
-    if (roomId === undefined) {
-      throw new WsException('방에 참가하지 않으셨습니다.');
-    }
-
     client.data.nickname = nickname.trim();
 
     this.server.to(roomId).emit('participant:info:update', { participantId: client.id, nickname });

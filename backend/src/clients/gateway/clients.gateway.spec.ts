769b3c169f63bcc2e6f38531fe061b7f9af6be51
eora21 <eora21@naver.com>
2024-11-18 20:33:45 +0900

    test: 💍 (clients.gateway.spec) 종속성 반영

--- backend/src/clients/gateway/clients.gateway.spec.ts
+++ backend/src/clients/gateway/clients.gateway.spec.ts
@@ -1,13 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ClientsGateway } from './clients.gateway';
-import { ClientsService } from '../service/clients.service';
+import { RoomsService } from '../../rooms/service/rooms.service';
+import { RoomsInMemoryRepository } from '../../rooms/repository/rooms.in-memory.repository';
 
 describe('ClientsGateway', () => {
   let gateway: ClientsGateway;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ClientsGateway, ClientsService],
+      providers: [ClientsGateway, RoomsService, RoomsInMemoryRepository],
     }).compile();
 
     gateway = module.get<ClientsGateway>(ClientsGateway);

6bdf9176d07f232e6bf7ea2ec026ad068e64f8f0
eora21 <eora21@naver.com>
2024-11-19 22:57:06 +0900

    feat: 🎸 PhaseInterestGuard 추가, 추상화를 통한 코드 개선

--- backend/src/common/guard/phase.keyword.guard.ts
+++ backend/src/common/guard/phase.keyword.guard.ts
@@ -1,22 +1,37 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { PHASE } from '../definition/phase';
 import { RoomsService } from '../../rooms/service/rooms.service';
+import { Phase, PHASE } from '../definition/phase';
 import { CustomException } from '../exception/custom-exception';
 
-@Injectable()
-export class PhaseKeywordGuard implements CanActivate {
-
-  constructor(private readonly roomsService: RoomsService) {
+export abstract class AbstractPhaseGuard implements CanActivate {
+  protected constructor(
+    private readonly roomsService: RoomsService,
+    private readonly phase: Phase
+  ) {
   }
 
   canActivate(context: ExecutionContext): boolean {
     const client = context.switchToWs().getClient();
     const roomId = client.data.roomId;
 
-    if (!this.roomsService.isPhase(roomId, PHASE.KEYWORD)) {
+    if (!this.roomsService.isPhase(roomId, this.phase)) {
       throw new CustomException('현재 방 상태에서 허용되지 않는 명령입니다.');
     }
 
     return true;
+  }
+}
+
+@Injectable()
+export class PhaseKeywordGuard extends AbstractPhaseGuard {
+  constructor(roomsService: RoomsService) {
+    super(roomsService, PHASE.KEYWORD);
+  }
+}
+
+@Injectable()
+export class PhaseInterestGuard extends AbstractPhaseGuard {
+  constructor(roomsService: RoomsService) {
+    super(roomsService, PHASE.INTEREST);
   }
 }

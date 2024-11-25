f63660bacfd38d6edcfbca26fbbf297be04ad5c0
eora21 <eora21@naver.com>
2024-11-25 16:07:39 +0900

    fix: 🐛 (PhaseGuard) 입장해있는 방 혹은 요청한 방을 통해 검증하도록 수정

--- backend/src/common/guard/phase.guard.ts
+++ backend/src/common/guard/phase.guard.ts
@@ -11,8 +11,9 @@ export abstract class AbstractPhaseGuard implements CanActivate {
   }
 
   canActivate(context: ExecutionContext): boolean {
-    const client = context.switchToWs().getClient();
-    const roomId = client.data.roomId;
+    const wsArguments = context.switchToWs();
+    const client = wsArguments.getClient();
+    const roomId = client.data.roomId ?? wsArguments.getData().roomId;
 
     if (!this.roomsService.isPhase(roomId, this.phase)) {
       throw new CustomException('현재 방 상태에서 허용되지 않는 명령입니다.');

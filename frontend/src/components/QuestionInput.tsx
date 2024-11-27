1d03781a7116fec2029cef9f0c6a609a68396dad
choyunhee <xornd1110@naver.com>
2024-11-27 11:20:53 +0900

    refactor: 💡 키워드 잘림 이슈 해결: 키워드 글자수 제한

--- frontend/src/components/QuestionInput.tsx
+++ frontend/src/components/QuestionInput.tsx
@@ -5,12 +5,17 @@ import { useToast } from '@/hooks';
 import { sendPickKeywordMessage } from '@/services';
 import { useSocketStore } from '@/stores';
 import { Variables } from '@/styles';
-import { KeywordResponse } from '@/types';
 
 interface QuestionInputProps {
   currentQuestionIndex: number;
   onSubmit: (keyword: string, type: 'add') => void;
 }
+
+const wrapperStyle = css`
+  position: relative;
+  width: 100%;
+  margin-top: ${Variables.spacing.spacing_sm};
+`;
 
 const inputStyle = css`
   width: 100%;
@@ -27,10 +32,19 @@ const inputStyle = css`
   opacity: 1;
 `;
 
+const spanStyle = css`
+  position: absolute;
+  right: 10px;
+  bottom: 5px;
+  font-size: 12px;
+  color: ${Variables.colors.text_alt};
+`;
+
 const QuestionInput = ({ currentQuestionIndex, onSubmit }: QuestionInputProps) => {
   const [keyword, setKeyword] = useState('');
   const { openToast } = useToast();
   const { socket } = useSocketStore();
+  const MAX_LENGTH = 15;
 
   async function handleEnter(e: React.KeyboardEvent) {
     if (e.code !== 'Enter') return;
@@ -48,19 +62,33 @@ const QuestionInput = ({ currentQuestionIndex, onSubmit }: QuestionInputProps) =
       } catch (error) {
         if (error instanceof Error) openToast({ text: error.message, type: 'error' });
       }
-
     }
   }
 
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+
+    // 입력 길이가 최대 길이를 초과하면 자름
+    if (value.length > MAX_LENGTH) {
+      setKeyword(value.slice(0, MAX_LENGTH));
+    } else {
+      setKeyword(value);
+    }
+  };
+
   return (
-    <input
-      type="text"
-      value={keyword}
-      placeholder="답변을 입력해주세요"
-      css={inputStyle}
-      onChange={(e) => setKeyword(e.target.value)}
-      onKeyDown={(e) => handleEnter(e)}
-    />
+    <div css={wrapperStyle}>
+      <input
+        type="text"
+        value={keyword}
+        placeholder="답변을 입력해주세요"
+        css={inputStyle}
+        maxLength={MAX_LENGTH}
+        onChange={handleInputChange}
+        onKeyDown={(e) => handleEnter(e)}
+      />
+      <span css={spanStyle}>{keyword.length}/15</span>
+    </div>
   );
 };
 

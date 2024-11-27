3abbf66582918b20f69941447b86e5fc829869de
23YUJIN <asdbnm066@daum.net>
2024-11-27 17:47:55 +0900

    feat: 🎸 받은 컨텐츠가 유튜브일 때  표시하기

--- frontend/src/pages/room/content-share/contentPresentSection.tsx
+++ frontend/src/pages/room/content-share/contentPresentSection.tsx
@@ -1,6 +1,9 @@
 import { css } from '@emotion/react';
 
 import { Content } from '@/types';
+import { Player } from '@/components';
+import { isShorts } from '@/utils';
+import { useSocketStore } from '@/stores';
 
 const ContentPresentSectionStyle = css({
   flexGrow: 1,
@@ -27,12 +30,22 @@ interface ContentPresentSectionProps {
 
 /* 컨텐츠가 실제로 표시되는 영역 */
 const ContentPresentSection = ({ content }: ContentPresentSectionProps) => {
+  const { socket } = useSocketStore();
+  const isSharer = content.sharerSocketId === socket.id;
+
   return (
-    content.type === 'IMAGE' && (
-      <div css={ContentPresentSectionStyle}>
-        <img css={ImageContentStyle} src={content.resourceURL} alt={'content'} />
-      </div>
-    )
+    <>
+      {content.type === 'IMAGE' && (
+        <div css={ContentPresentSectionStyle}>
+          <img css={ImageContentStyle} src={content.resourceURL} alt={'content'} />
+        </div>
+      )}
+      {content.type === 'YOUTUBE' && (
+        <div css={ContentPresentSectionStyle}>
+          <Player url={content.resourceURL} isSharer={isSharer} isShorts={isShorts(content.resourceURL)} />
+        </div>
+      )}
+    </>
   );
 };
 

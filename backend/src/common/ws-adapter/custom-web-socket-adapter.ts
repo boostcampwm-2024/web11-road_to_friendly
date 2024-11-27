3cf57049afb4f7d656fb43cde063be13407e5926
eora21 <eora21@naver.com>
2024-11-27 19:17:54 +0900

    feat: 🎸 RedisAdapter 사용

--- backend/src/common/ws-adapter/custom-web-socket-adapter.ts
+++ backend/src/common/ws-adapter/custom-web-socket-adapter.ts
@@ -1,14 +1,25 @@
 import { IoAdapter } from '@nestjs/platform-socket.io';
-import { ServerOptions } from 'socket.io';
+import { Server, ServerOptions } from 'socket.io';
+import * as process from 'node:process';
+import Redis from 'ioredis';
+import { createAdapter } from '@socket.io/redis-adapter';
 
 export class CustomWebSocketAdapter extends IoAdapter {
+  create(port: number, options?: ServerOptions & { namespace?: string; server?: any }): Server {
+    return super.create(port, options);
+  }
+
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   createIOServer(port: number, options?: ServerOptions & { namespace?: string; server?: any }): any {
+    const pubClient = new Redis();
+    const subClient = pubClient.duplicate();
+
     return super.createIOServer(port, {
       ...options,
       cors: {
         origin: process.env.ORIGIN,
       },
+      adapter: createAdapter(pubClient, subClient),
     });
   }
 }

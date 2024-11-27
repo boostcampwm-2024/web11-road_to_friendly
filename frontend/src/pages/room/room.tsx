53ed8f8aface802c4b894ddd135bbe82aff11ad1
choyunhee <xornd1110@naver.com>
2024-11-27 11:42:33 +0900

    refactor: 💡 setOutOfBounds(참여자 사라짐)메서드 실행 위치 조정

--- frontend/src/pages/room/room.tsx
+++ frontend/src/pages/room/room.tsx
@@ -53,6 +53,7 @@ const Room = () => {
 
   const { participants, hostId, currentUserId, roomExists } = useParticipants(roomId, setInitialLoading);
   const { radius, increaseRadius, increaseLongRadius } = useRadiusStore();
+  const { setOutOfBounds } = useRadiusStore();
 
   const [isIntroViewActive, setIsIntroViewActive] = useState(true);
   const [isResultView, setIsResultView] = useState(false); //결과 페이지 여부
@@ -74,7 +75,10 @@ const Room = () => {
     [radius, participants]
   );
 
-  const hideIntroView = () => setIsIntroViewActive(false);
+  const hideIntroView = () => {
+    setIsIntroViewActive(false);
+    setOutOfBounds(true);
+  };
 
   const calculateRadius = (count: number) => {
     if (count > 3) {

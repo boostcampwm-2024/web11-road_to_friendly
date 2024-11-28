b321a42956e15ff789430acb90cc841a74fb07a2
choyunhee <xornd1110@naver.com>
2024-11-28 21:17:36 +0900

    style: 💄 타원의 반지름을 화면크기에 비례하게 설정

--- frontend/src/constants/radius.ts
+++ frontend/src/constants/radius.ts
@@ -1,7 +1,8 @@
-export const MIN_SHORT_RADIUS = 210; //반지름
-export const MIN_LONG_RADIUS = Math.floor((MIN_SHORT_RADIUS * 4) / 3); //장축:단축 = 4:3
-export const MAX_SHORT_RADIUS = 220;
-export const MAX_LONG_RADIUS = Math.floor((MAX_SHORT_RADIUS * 4) / 3);
+export const MIN_SHORT_RADIUS = Math.floor(window.innerHeight * 0.3); // 화면 높이의 30%로 최소 반지름 설정
+export const MIN_LONG_RADIUS = Math.floor((MIN_SHORT_RADIUS * 4) / 3); // 비율에 맞춰 장축 계산
+
+export const MAX_SHORT_RADIUS = Math.floor(window.innerHeight * 0.33); // 화면 높이의 35%로 최대 반지름 설정
+export const MAX_LONG_RADIUS = Math.floor((MAX_SHORT_RADIUS * 4) / 3); //장축:단축 = 4:3
 
 export const BIG_THRESHOLD = 20;
 export const MIDEIUM_THRESHOLD = 40;

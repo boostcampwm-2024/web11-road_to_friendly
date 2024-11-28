5bd4ed08feb77def6030e820a65637db2a421eb7
Jongmin Lee <em11133@naver.com>
2024-11-28 15:44:12 +0900

    refactor: 공유큐 , 이미지 레포지토리 분리

---
+++ backend/src/interests/repository/interests.in-memory.image.repository.ts
@@ -0,0 +1,27 @@
+import { join } from 'path';
+import { promises as fs } from 'fs';
+
+import { v4 as uuid } from 'uuid';
+
+import { CustomException } from '../../common/exception/custom-exception';
+import { InterestsImageDto } from '../dto/interests.image.dto';
+import { InterestsImageRepository } from './interests.image.repository';
+
+export class InterestsInMemoryImageRepository implements InterestsImageRepository {
+  async uploadImage(data: InterestsImageDto) {
+    const uniqueFileName = `${uuid()}-${data.fileName}`;
+
+    const dirPath = join(process.cwd(), 'src', 'interests', 'shareImage');
+    const filePath = join(dirPath, uniqueFileName);
+
+    try {
+      await fs.mkdir(dirPath, { recursive: true });
+
+      await fs.writeFile(filePath, data.buffer);
+
+      return `http://localhost:8080/shareImage/${uniqueFileName}`;
+    } catch (error) {
+      throw new CustomException(`파일 업로드 실패: ${error.message}`);
+    }
+  }
+}
--- backend/src/interests/repository/interests.repository.ts
+++ backend/src/interests/repository/interests.repository.ts
@@ -1,10 +1,8 @@
 import { Interest } from '../domain/interest';
-import { InterestsImageDto } from '../dto/interests.image.dto';
 import { InterestsBroadcastResponseDto } from '../dto/interests.broadcast.response.dto';
 
 export interface InterestsRepository {
   addInterestIfBroadcasting(roomId: string, interest: Interest): InterestsBroadcastResponseDto;
   next(roomId: string, hostFlag: boolean, clientId: string): InterestsBroadcastResponseDto;
   deleteRoomInterest(roomId: string): void;
-  uploadImage(data: InterestsImageDto): Promise<string>;
 }

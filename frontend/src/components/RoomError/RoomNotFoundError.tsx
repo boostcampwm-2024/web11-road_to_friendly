ca87f809b046e39396eca0e1d6b4bc0afb8c5759
23YUJIN <asdbnm066@daum.net>
2024-12-02 23:56:15 +0900

    feat: 🎸 RoomError 컴포넌트들에 Lottie 애니메이션 추가

--- frontend/src/components/RoomError/RoomNotFoundError.tsx
+++ frontend/src/components/RoomError/RoomNotFoundError.tsx
@@ -1,4 +1,5 @@
 import RoomError from './RoomError';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 const RoomNotFoundError = () => {
   return (
@@ -6,6 +7,15 @@ const RoomNotFoundError = () => {
       title="방 찾을 수 없음"
       description="죄송합니다. 요청하신 방은 존재하지 않습니다.
       입력한 방 번호가 정확한지 확인해 주시고, 다시 시도해 주세요."
+      AnimationElement={
+        <div css={{ width: '40rem' }}>
+          <DotLottieReact
+            src="https://lottie.host/091cc406-9d06-4f3c-b8c9-1bec84ed5ecf/CA1ZTxcqlY.lottie"
+            loop
+            autoplay
+          />
+        </div>
+      }
     />
   );
 };

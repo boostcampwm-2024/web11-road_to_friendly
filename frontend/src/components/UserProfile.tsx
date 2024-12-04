eaa0e1b710ea84611551a049d1d1c2121e014a30
choyunhee <xornd1110@naver.com>
2024-12-04 16:26:25 +0900

    refactor: 💡 React.memo를 userProfile에 적용해 props가 변경될때만 리렌더링되도록 함

--- frontend/src/components/UserProfile.tsx
+++ frontend/src/components/UserProfile.tsx
@@ -1,4 +1,5 @@
 import { css } from '@emotion/react';
+import React from 'react';
 
 import Crown from '@/assets/icons/crown.svg?react';
 import { PROFILE_STYLES } from '@/constants/profile';
@@ -6,6 +7,54 @@ import ResultView from '@/pages/room/resultView';
 import { useRadiusStore } from '@/stores';
 import { Variables, flexStyle } from '@/styles';
 import { Participant } from '@/types';
+
+interface Positon {
+  x: number;
+  y: number;
+}
+
+interface UserProfileProps {
+  participant: Participant;
+  isCurrentUser: boolean;
+  isHost: boolean;
+  position: Positon;
+  isResultView: boolean;
+}
+
+const UserProfile = React.memo(
+  ({ participant, isCurrentUser, isHost, position, isResultView }: UserProfileProps) => {
+    const { radius, isOutOfBounds } = useRadiusStore();
+    const profileStyles = PROFILE_STYLES[(participant?.index || 0) % PROFILE_STYLES.length];
+
+    return (
+      <div css={profileStyle(position.x, position.y, radius[0], radius[1], isOutOfBounds)}>
+        <div css={profileImageStyle(profileStyles[0])}>
+          {isHost && (
+            <div css={hostStyle}>
+              <Crown />
+            </div>
+          )}
+          <div>{profileStyles[1]}</div>
+          <div css={participantNicknameStyle}>{participant.nickname}</div>
+          {isCurrentUser && <div css={selfTagStyle}>나</div>}
+        </div>
+        {isResultView && <ResultView participant={participant} />}
+      </div>
+    );
+  },
+  (prevProps, nextProps) => {
+    return (
+      prevProps.participant.nickname === nextProps.participant.nickname &&
+      prevProps.participant.index === nextProps.participant.index &&
+      prevProps.isCurrentUser === nextProps.isCurrentUser &&
+      prevProps.isHost === nextProps.isHost &&
+      prevProps.position.x === nextProps.position.x &&
+      prevProps.position.y === nextProps.position.y &&
+      prevProps.isResultView === nextProps.isResultView
+    );
+  }
+);
+export default UserProfile;
 
 const profileStyle = (x: number, y: number, shortRadius: number, longRadius: number, isOutOfBounds: boolean) => css`
   position: absolute;
@@ -61,39 +110,3 @@ const hostStyle = css`
   width: 50px;
   height: 40px;
 `;
-
-interface Positon {
-  x: number;
-  y: number;
-}
-
-interface UserProfileProps {
-  participant: Participant;
-  isCurrentUser: boolean;
-  isHost: boolean;
-  position: Positon;
-  isResultView: boolean;
-}
-
-const UserProfile = ({ participant, isCurrentUser, isHost, position, isResultView }: UserProfileProps) => {
-  const { radius, isOutOfBounds } = useRadiusStore();
-  const profileStyles = PROFILE_STYLES[(participant?.index || 0) % PROFILE_STYLES.length];
-
-  return (
-    <div css={profileStyle(position.x, position.y, radius[0], radius[1], isOutOfBounds)}>
-      <div css={profileImageStyle(profileStyles[0])}>
-        {isHost && (
-          <div css={hostStyle}>
-            <Crown />
-          </div>
-        )}
-        <div>{profileStyles[1]}</div>
-        <div css={participantNicknameStyle}>{participant.nickname}</div>
-        {isCurrentUser && <div css={selfTagStyle}>나</div>}
-      </div>
-      {isResultView && <ResultView participant={participant} />}
-    </div>
-  );
-};
-
-export default UserProfile;

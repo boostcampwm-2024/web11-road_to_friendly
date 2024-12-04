2485af5c369a17adc20b7d358497d31d8d3ea4e2
23YUJIN <asdbnm066@daum.net>
2024-12-05 07:41:11 +0900

    refactor: 💡 불필요한 구버전 중복 방 입장 코드 삭제

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -3,6 +3,5 @@ export { useFraction } from './useFraction';
 export { useToast } from './useToast';
 export { useIndex } from './useIndex';
 export { useTimeout } from './useTimeout';
-export { useRoomAccess } from './useRoomAcess';
 export { useLoadingState } from './useLoadingState';
 export { useViewState } from './useViewState';
